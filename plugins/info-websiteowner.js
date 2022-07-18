96ca555c64ff18532cbfea986de6e25b62f922af
andhyca-offc <108988560+andhyca-offc@users.noreply.github.com>
2022-07-18 22:01:10 +0700

    Update info-websiteowner.js

--- plugins/info-websiteowner.js
+++ plugins/info-websiteowner.js
@@ -4,12 +4,12 @@ let handler = async (m, { conn }) => {
     /*conn.sendTemplateButtonDoc(m.chat, col, `*Link Group:* ${await conn.getName(m.chat)}\n\nhttps://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat), wm, 'Info Grup', '.infogrup', m)*/
 
 conn.sendMessage(m.chat, {
-  text: `Fangz BOT Website`,
+  text: `ANDHYCABOTZ-MD Website`,
   templateButtons: [{
     index: 1,
     urlButton: {
-      displayText: `Link`,
-      url: 'https://fangzbot.websites.co.in/'
+      displayText: `📸 Instagram Owner`,
+      url: 'https://instagram.com/andhycarstofficial'
     }
   }],
   footer: wm

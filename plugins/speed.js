4fdb19c7d345fb9dcadca96734488a9447dea6b9
andhyca-offc <108988560+andhyca-offc@users.noreply.github.com>
2022-07-18 21:49:59 +0700

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -80,7 +80,7 @@ ${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type =>
 
 _CPU Core(s) Usage (${cpus.length} Core CPU)_
 ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
-`,botdate, null, sgc, 'YOUTUBE BOT', null,null, [[null,null],[null,null],[null,null]], m)
+`,botdate, null, sgc, '💌 Group Official', null,null, [[null,null],[null,null],[null,null]], m)
 }
 handler.help = ['ping', 'speed']
 handler.tags = ['info', 'tools']

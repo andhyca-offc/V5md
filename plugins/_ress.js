88d3e31d93a635045788de9313834d442e097995
andhyca-offc <108988560+andhyca-offc@users.noreply.github.com>
2022-07-19 09:43:29 +0700

    Update _ress.js

--- plugins/_ress.js
+++ plugins/_ress.js
@@ -6,7 +6,7 @@ import { readFileSync } from "fs"
 export async function all(m) {
 
   //Kalo mau menggokil pake ini
-  let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+  let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/935b88bd02274c6bfdb7c.jpg')
 
   let stc = readFileSync('./thumbnail.jpg')
 
@@ -18,10 +18,10 @@ if (m.isBaileys) return
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.sendMessage(m.chat, { sticker : stc, thumbnail: await( await fetch(pp)).buffer() , contextInfo:{  externalAdReply: { showAdAttribution: true,
 mediaType:  1,
-mediaUrl: 'https://wa.me/6288215689772',
+mediaUrl: 'https://wa.me/6285872761910',
 title: '「 ❔ 」',
 body: wm,
-sourceUrl: 'https://fangzbot.websites.co.in/', thumbnail: await( await fetch(pp)).buffer()
+sourceUrl: 'https://instagram.com/andhycarsroffixial', thumbnail: await( await fetch(pp)).buffer()
  //Kalo mau ada pp dari sender/pengirimnya
  //Kalo mau ringan pake global.thumb                                                                                                              
                                                                                                                
